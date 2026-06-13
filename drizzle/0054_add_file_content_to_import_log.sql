1cc1b4947b8d4a06557227d8192b974e47ee727b
Alan Racek <alanracek@gmail.com>
2026-06-12 20:12:06 -0700

    db fix

--- drizzle/0054_add_file_content_to_import_log.sql
+++ drizzle/0054_add_file_content_to_import_log.sql
@@ -1,2 +1,1 @@
-ALTER TABLE "import_log" ADD COLUMN "file_content" text;--> statement-breakpoint
-ALTER TABLE "plaid_connections" ADD COLUMN "disabled_accounts" jsonb DEFAULT '[]'::jsonb;
+ALTER TABLE "import_log" ADD COLUMN IF NOT EXISTS "file_content" text;
